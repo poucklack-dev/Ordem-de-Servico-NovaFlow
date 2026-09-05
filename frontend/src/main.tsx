57804a34218d7c63f7712ec1b970df62000a6687
poucklack-dev <poucklack@gmail.com>
2026-09-05 06:28:12 -0300

    style: atualiza tema para preto grafite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,2 +1,2 @@
-import{createRoot}from'react-dom/client';import App from'./App';import'./style.css';import'./app.css';import'./filters.css';
+import{createRoot}from'react-dom/client';import App from'./App';import'./style.css';import'./app.css';import'./filters.css';import'./graphite.css';
 createRoot(document.getElementById('root')!).render(<App/>);
